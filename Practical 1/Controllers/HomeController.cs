cba4346d8b608eaab68ab495139be5659a608065
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 00:05:04 +0530

    Add files via upload

---
+++ Practical 1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practical1MVC.Models;
+
+namespace Practical1MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new AddModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(AddModel model)
+        {
+            model.Result = model.A + model.B;
+            return View(model);
+        }
+    }
+}
---
+++ Practical 1/Models/AddModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Practical1MVC.Models
+{
+    public class AddModel
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int Result { get; set; }
+    }
+}
