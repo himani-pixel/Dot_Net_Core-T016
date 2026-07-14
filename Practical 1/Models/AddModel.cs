78d1d841e29aa9f304ebd0b9a5bde0b40cefd45a
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 00:02:22 +0530

    Delete Practical 1/Models directory

--- Practical 1/Models/AddModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Practical1MVC.Models
-{
-    public class AddModel
-    {
-        public int A { get; set; }
-        public int B { get; set; }
-        public int Result { get; set; }
-    }
-}
