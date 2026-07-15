30b7493509c51a044d1f0ac10bf2b2b1a420a8b1
Himani Malankar <himanimalankar@gmail.com>
2026-07-15 20:02:43 +0530

    Update Practical_1C.cs

--- Practical 1/Practical_1C.cs
+++ Practical 1/Practical_1C.cs
@@ -1,23 +1,23 @@
-﻿//using System;
+﻿using System;
 
-//class Practical_1C
-//{
-//    public static void Main(string[] args)
-//    {
-//        int count = 1;
+class Practical_1C
+{
+   public static void Main(string[] args)
+   {
+       int count = 1;
 
-//        for (int i = 1; i <= 5; i++)
-//        {
-//            for (int j = 1; j <= i; j++)
-//            {
-//                Console.Write(count + " ");
-//                count++;
-//            }
+       for (int i = 1; i <= 5; i++)
+       {
+           for (int j = 1; j <= i; j++)
+           {
+               Console.Write(count + " ");
+               count++;
+           }
 
-//            Console.WriteLine();
-//        }
+           Console.WriteLine();
+       }
 
-//        Console.WriteLine("Himani Malankar T016");
-//        Console.ReadLine();
-//    }
-//}
+       Console.WriteLine("Himani Malankar T016");
+       Console.ReadLine();
+   }
+}
