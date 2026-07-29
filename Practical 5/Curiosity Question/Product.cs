79bfc34784679f720c19632785b4e6ed758185f8
Himani Malankar <himanimalankar@gmail.com>
2026-07-29 14:58:54 +0530

    Create Product.cs

---
+++ Practical 5/Curiosity Question/Product.cs
@@ -0,0 +1,11 @@
+namespace Pratical_5_CQ.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
