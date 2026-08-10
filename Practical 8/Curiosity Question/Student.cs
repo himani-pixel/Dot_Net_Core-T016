8dcd76d2dc87283eff6b53a8233172476fc8fd2e
Himani Malankar <himanimalankar@gmail.com>
2026-08-10 20:09:51 +0530

    Create Student.cs

---
+++ Practical 8/Curiosity Question/Student.cs
@@ -0,0 +1,11 @@
+namespace Practical_8_CQm.Models
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+
+        public string Name { get; set; }
+
+        public string Course { get; set; }
+    }
+}
